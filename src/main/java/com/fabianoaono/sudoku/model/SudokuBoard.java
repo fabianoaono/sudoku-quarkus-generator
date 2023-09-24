3b343d83a81d0d68b02051182f1f88e643cdf7c7
Fabiano Aono <fabianoaono@gmail.com>
2023-09-23 22:31:53 -0300

    Adding OpenApi documentation on Sudoku service

--- src/main/java/com/fabianoaono/sudoku/model/SudokuBoard.java
+++ src/main/java/com/fabianoaono/sudoku/model/SudokuBoard.java
@@ -1,11 +1,22 @@
 package com.fabianoaono.sudoku.model;
 
 import com.fabianoaono.sudoku.config.Difficulty;
+import jakarta.ws.rs.FormParam;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
 
 public class SudokuBoard {
 
     private int[][] values;
     private int[][] solution;
+
+    @Parameter(
+            description = "Sudoku board difficulty to create",
+            name="difficulty",
+            required = true
+    )
     private Difficulty difficulty;
 
     public SudokuBoard(int[][] values, int[][] solution, Difficulty difficulty) {
--- src/main/java/com/fabianoaono/sudoku/resource/SudokuResource.java
+++ src/main/java/com/fabianoaono/sudoku/resource/SudokuResource.java
@@ -10,13 +10,22 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/sudoku")
+@Tag(name="Sudoku Resouce", description="Sudoku Rest APIs")
 public class SudokuResource {
 
     public static class Parameters {
         @RestQuery
+        @Parameter(description = "Difficulty to generate")
         Difficulty difficulty;
     }
     @Inject
@@ -24,6 +33,16 @@ public class SudokuResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            operationId = "generateBoard",
+            summary = "Generates a random sudoku board",
+            description = "Generates a random sudoku board according to the parameters"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Operation completed",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
     public Response generateBoard(@BeanParam Parameters parameters) {
         SudokuBoard generatedBoard = sudokuService.generateSudokuBoard(parameters.difficulty);
         return Response.ok(generatedBoard).build();
